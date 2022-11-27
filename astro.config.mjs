5f2c81c3aab578107afd606c95501b534c8ef95c
Milos Djakovic <milos.g.djakovic@gmail.com>
2022-11-28 00:08:19 +0100

    Vue framework added

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,11 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/integrations-guide/sitemap/#usage
   site: 'http://milanpetkovic.com',
-  integrations: [tailwind(), image(), sitemap()]
+  integrations: [tailwind(), image(), sitemap(), vue()]
 });
