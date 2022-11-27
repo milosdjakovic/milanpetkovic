dd05396d49e48529c552ac562f6ca5dafa278f01
Milos Djakovic <milos.g.djakovic@gmail.com>
2022-11-27 16:07:09 +0100

    Get language now returns Lang type

--- src/bin/language.ts
+++ src/bin/language.ts
@@ -8,8 +8,8 @@ export function initializeLanguage(): void {
   localStorage.setItem("language", "en");
 }
 
-export function getLanguage(): string {
-  return localStorage.getItem("language") ?? 'en';
+export function getLanguage(): Lang {
+  return localStorage.getItem("language") as Lang ?? 'en';
 }
 
 export function setLanguage(language: Lang): void {
